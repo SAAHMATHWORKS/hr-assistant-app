8f8a658c8ff535c9f3cce7611394baf5bbdd86e3
SAAHMATHWORKS <saahmathworks@gmail.com>
2025-06-21 13:02:31 +0100

    fix chatOpenAI

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+# from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
